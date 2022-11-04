cffe3c24c930adc634702e080c0eb89fb8007553
Joohyung Park <johy@dabyeol.com>
2022-11-04 22:15:40 +0900

    Remove test code

--- opencv/turbidity.py
+++ opencv/turbidity.py
@@ -19,14 +19,4 @@ cv.imshow("Low", dst)
 height, width = dst.shape[:2]
 print("Low", cv.countNonZero(dst) / (height * width))
 
-
-'''                 <For test>
-src = cv.imread("C:/Users/64527/Downloads/test_img.png")
-src_hsv = cv.cvtColor(src, cv.COLOR_BGR2HSV)
-dst = cv.inRange(src_hsv, lowerBound, upperBound)
-cv.imshow("test", dst)
-cv.imshow("test_src",src)
-height, width = dst.shape[:2]
-print("test", cv.countNonZero(dst) / (height * width))
-'''
 cv.waitKey()
