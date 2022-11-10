3f181ce9d14312e5393b015b05f9fcb467515b03
Joohyung Park <johy@dabyeol.com>
2022-11-10 19:45:40 +0900

    Fix image location

--- opencv/turbidity.py
+++ opencv/turbidity.py
@@ -2,45 +2,45 @@ import cv2 as cv
 
 lowerBound = (30, 0, 0)
 upperBound = (80, 255, 200)
+
 # High turbidity
-src = cv.imread("C:/Users/64527/Desktop/Smart_CCTV/humen_detect_test/2022ESWContest_webOS_3021/opencv/img/high.jpg")
-if src is None: raise Exception("Could not load image")
-src = cv.resize(src, (640, 480), interpolation= cv.INTER_CUBIC)
+src = cv.imread("opencv/img/high.jpg")
+if src is None:
+    raise Exception("Could not load image")
+src = cv.resize(src, (640, 480), interpolation=cv.INTER_CUBIC)
 src_hsv = cv.cvtColor(src, cv.COLOR_BGR2HSV)
 H_dst = cv.inRange(src_hsv, lowerBound, upperBound)
 height, width = H_dst.shape[:2]
 pixel_cnt = (height * width)
-#divide the image BGR into 3 parts
+# divide the image BGR into 3 parts
 b, g, r = cv.split(src)
-#multiply the image BGR by the mask
+# multiply the image BGR by the mask
 H_g = cv.bitwise_and(g, H_dst)
-#sum the pixels of the image
+# sum the pixels of the image
 H_g_sum = cv.sumElems(H_g)
-#calculate the average
+# calculate the average
 print("High = ", H_g_sum[0]/pixel_cnt)
 
 # Low turbidity
-src = cv.imread("C:/Users/64527/Desktop/Smart_CCTV/humen_detect_test/2022ESWContest_webOS_3021/opencv/img/low.jpg")
-src = cv.resize(src, (640, 480), interpolation= cv.INTER_CUBIC)
+src = cv.imread("opencv/img/low.jpg")
+src = cv.resize(src, (640, 480), interpolation=cv.INTER_CUBIC)
 src_hsv = cv.cvtColor(src, cv.COLOR_BGR2HSV)
 L_dst = cv.inRange(src_hsv, lowerBound, upperBound)
 height, width = L_dst.shape[:2]
 pixel_cnt = (height * width)
-#divide the image BGR into 3 parts
+# divide the image BGR into 3 parts
 b, g, r = cv.split(src)
-#multiply the image BGR by the mask
+# multiply the image BGR by the mask
 L_g = cv.bitwise_and(g, L_dst)
-#sum all pixels
+# sum all pixels
 L_g_sum = cv.sumElems(L_g)
-#average pixels
+# average pixels
 print("Low =", L_g_sum[0] / pixel_cnt)
 
-#stack the images
+# stack the images
 stack_dst = cv.hconcat([H_dst, L_dst])
-
 stack_g = cv.hconcat([H_g, L_g])
 image = cv.vconcat([stack_dst, stack_g])
 cv.imshow("stack", image)
-
 
 cv.waitKey()
