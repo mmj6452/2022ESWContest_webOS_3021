031c9c68d31618c4e0711e7390ba09e2b2009142
mmj6452 <645277@naver.com>
2022-11-27 01:14:01 +0900

    express_ws_add

---
+++ express-ws/app.js
@@ -0,0 +1,27 @@
+
+const port = 3000;
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+
+app.use(function (req, res, next) {
+  console.log('middleware');
+  req.testing = 'testing';
+  return next();
+});
+
+app.get('/', function(req, res, next){
+  console.log('get route', req.testing);
+  res.end();
+});
+
+app.ws('/', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+  console.log('socket', req.testing);
+});
+
+app.listen(port,() => {
+  console.log(`Server started on port ${port}`);
+});
